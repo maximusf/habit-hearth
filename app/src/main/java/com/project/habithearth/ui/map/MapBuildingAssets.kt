9b806368684e2d771e92aefca14919553a49fc18
QuietKat329 <kgturner0909@gmail.com>
2026-04-28 17:48:06 -0400

    Changed the cottage image path

--- app/src/main/java/com/project/habithearth/ui/map/MapBuildingAssets.kt
+++ app/src/main/java/com/project/habithearth/ui/map/MapBuildingAssets.kt
@@ -20,7 +20,7 @@ private val BuildingMarkerArtById: Map<String, BuildingMarkerArt> = mapOf(
         image = "images/buildings/wisdom/observatory.PNG",
     ),
     "cottage" to BuildingMarkerArt(
-        image = "images/buildings/vitality/hotspring.PNG",
+        image = "images/buildings/cottage.png",
     ),
     "spa" to BuildingMarkerArt(
         image = "images/buildings/vitality/greenhouse.PNG",
